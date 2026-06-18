99690c6b53b0230c0c3eec197cabf5f69f10f402
chienpv109 <chienboybu1092004@gmail.com>
2026-06-18 21:34:18 +0700

    change domain

--- Program.cs
+++ Program.cs
@@ -25,7 +25,8 @@ builder.Services.AddCors(options =>
                 "http://localhost:5174",
                 "http://localhost:5175",
                 "http://localhost:5176",
-                "http://localhost:3000"
+                "http://localhost:3000",
+                "https://quiz-system-fe-eta.vercel.app"
               )
               .AllowAnyMethod()
               .AllowAnyHeader();
